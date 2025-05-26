319797ff11e5dd448134bb99bfca4e44bae2dec8
Mettzy <muhammadnaufal.rajwatuzzaman@gmail.com>
2025-05-27 06:58:45 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("belajar gelut")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "mari belajar gelut bersama naufal razwa"
 )
